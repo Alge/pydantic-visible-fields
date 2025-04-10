6169ac9942bdeb93297c3e5757579bff45e17010
Martin Alge <alge@46elks.com>
2025-04-10 15:54:36 +0200

    Bump version: 0.2.3 → 0.2.4

--- src/pydantic_visible_fields/__init__.py
+++ src/pydantic_visible_fields/__init__.py
@@ -8,7 +8,7 @@ from pydantic_visible_fields.core import (
     visible_fields_response,
 )
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __all__ = [
     "VisibleFieldsMixin",
     "VisibleFieldsModel",
